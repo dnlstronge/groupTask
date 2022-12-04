365c7b9a963c0a9163adbef3096e3bddcac7510a
Dan Stronge <dnlstronge@gmail.com>
2022-12-04 12:45:24 +0000

    create: add imports for each page prior to route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+
+// styles
+
 import './App.css'
 
+// pages 
+
+import Dashboard from './pages/dashboard/Dashboard'
+import Create from './pages/create/Create'
+import Login from './pages/login/Login';
+import Project from './pages/project/Project'
+import Signup from './pages/signup/Signup'
+ 
 function App() {
   return (
     <div className="App">
