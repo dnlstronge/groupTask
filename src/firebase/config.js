168b26584df73d400bac43e460cbc6b3f49aed76
Dan Stronge <dnlstronge@gmail.com>
2022-12-04 11:34:13 +0000

    import firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBORa1hioH8DGTupDQNlfiFbpWbD9mFKDg",
+    authDomain: "grouptaskapp-4eccd.firebaseapp.com",
+    projectId: "grouptaskapp-4eccd",
+    storageBucket: "grouptaskapp-4eccd.appspot.com",
+    messagingSenderId: "247010408403",
+    appId: "1:247010408403:web:2601d61aedaa9908026126"
+  };
