ba656da26498921f5a11f4eb59f582834e84d5e9
Dan Stronge <dnlstronge@gmail.com>
2022-12-05 08:24:55 +0000

    create: added links and login button to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,8 @@
+//imports
+
+import { Link } from 'react-router-dom'
+
+
 // styles + assets
 import Temple from '../assets/temple.svg'
 import './Navbar.css'
@@ -6,7 +11,15 @@ import './Navbar.css'
 export default function Navbar() {
   return (
     <div>
+        <ul>
+            <li className="logo"></li>
+            <img src={Temple} alt="logo"/>
+            <span>groupTask</span>
 
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/signup">Signup</Link></li>
+            <li><button className="btn">Logout</button></li>
+        </ul>
     </div>
   )
 }
