77b50a65d5431e53c0cccf6418635a0629cff1d7
Dan Stronge <dnlstronge@gmail.com>
2022-12-05 08:49:56 +0000

    fix: fixed closing of li tag to correctly position links

--- src/App.js
+++ src/App.js
@@ -12,12 +12,15 @@ import Create from './pages/create/Create'
 import Login from './pages/login/Login';
 import Project from './pages/project/Project'
 import Signup from './pages/signup/Signup'
+import Navbar from './components/Navbar';
  
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <div className="container">
+          <Navbar />
+
           <Switch>
             <Route exact path='/'>
               <Dashboard />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,9 +12,9 @@ export default function Navbar() {
   return (
     <div className="navbar">
         <ul>
-            <li className="logo"></li>
+            <li className="logo">
             <img src={Temple} alt="logo"/>
-            <span>groupTask</span>
+            <span>groupTask</span></li>
 
             <li><Link to="/login">Login</Link></li>
             <li><Link to="/signup">Signup</Link></li>
