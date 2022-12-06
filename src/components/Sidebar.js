a29a73246c45d685c47f093f4423779e69dbeb0a
Dan Stronge <dnlstronge@gmail.com>
2022-12-06 08:37:15 +0000

    create: new sidebar component import images

---
+++ src/components/Sidebar.js
@@ -0,0 +1,14 @@
+// styles & images
+
+
+import DashboardIcon from '../assets/dashboard_icon.svg'
+import AddIcon from '../assets/add_icon.svg'
+
+
+export default function Sidebar() {
+  return (
+    <div>
+
+    </div>
+  )
+}
