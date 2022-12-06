71d7a07da5327e350af9e33691dfd5f5b8c1a2f9
Dan Stronge <dnlstronge@gmail.com>
2022-12-06 08:53:00 +0000

    create: add nav links

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,3 +1,6 @@
+import { NavLink } from 'react-router-dom'
+
+
 // styles & images
 
 
@@ -7,8 +10,27 @@ import AddIcon from '../assets/add_icon.svg'
 
 export default function Sidebar() {
   return (
-    <div>
-
+    <div className="sidebar">
+      <div className="sidebar-content">
+        <div className="user">
+          {/* Avatar/username to be added later */}
+          <p>Hello user</p>
+        </div>
+        <nav className="links">
+          <ul>
+            <li>
+              <NavLink to="/"></NavLink>
+              <img src={DashboardIcon} alt="dashboard icon"/>
+              <span>Dashboard</span>
+            </li>
+            <li>
+              <NavLink to="/create"></NavLink>
+              <img src={AddIcon} alt="add project icon"/>
+              <span>New project</span>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   )
 }
