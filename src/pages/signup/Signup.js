92044063acab021345710f9586157695d0fb7c0b
Dan Stronge <dnlstronge@gmail.com>
2022-12-06 11:02:39 +0000

    create: state for sign up form inputs

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,13 +1,17 @@
 // styles
 
+import { useState } from 'react'
 import styles from './Signup.css'
 
 
 
 export default function Signup() {
+  const [ email, setEmail ] = useState('')
+  const [ password, setPassword ] = useState('')
+  const [ displayName, setDisplayName ] = useState('')
   return (
     <div>
-    Signup
+    
     </div>
   )
 }
