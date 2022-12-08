c6c9517493c6d22c83a91a4502c51f5e3d704eaf
Dan Stronge <dnlstronge@gmail.com>
2022-12-08 10:18:33 +0000

    create: add logout function, hook up uselogout test: users online status in firebase should change boolean value upon login/logout & signup (clear)

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,12 +1,13 @@
 import { useState, useEffect } from 'react'
-import { projectAuth } from '../firebase/config'
-import { useAuthContext } from './useAuthContext'
+import { projectAuth, projectStorage, projectFirestore } from '../firebase/config'
+import { useAuthContext, } from './useAuthContext'
+
 
 export const useLogin = () => {
   const [isCancelled, setIsCancelled] = useState(false)
   const [error, setError] = useState(null)
   const [isPending, setIsPending] = useState(false)
-  const { dispatch } = useAuthContext()
+  const { dispatch, user } = useAuthContext()
 
   const login = async (email, password) => {
     setError(null)
@@ -15,6 +16,10 @@ export const useLogin = () => {
     try {
       // login
       const res = await projectAuth.signInWithEmailAndPassword(email, password)
+      
+      // create a user document
+      const { uid } = projectAuth.currentUser
+      await projectFirestore.collection('users').doc(uid).update({ online: true })
 
       // dispatch login action
       dispatch({ type: 'LOGIN', payload: res.user })
--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { useSignup } from '../../hooks/useSignup'
+import { useLogin } from '../../hooks/useLogin'
 
 // styles
 
@@ -9,10 +9,11 @@ import styles from './Login.css'
 export default function Login() {
   const [ email, setEmail ] = useState('')
   const [ password, setPassword ] = useState('')
+  const { login, error, isPending } = useLogin()
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(email, password)
+    login(email, password)
   }
   
   return (
@@ -37,9 +38,9 @@ export default function Login() {
         />
       </label>
       
-      {/*!isPending && <button className="btn">Login</button>}
+      {!isPending && <button className="btn">Login</button>}
       {isPending && <button className="btn" disabled>Loading...</button>}
-      {error && <div className="error">{error}</div> */}
+      {error && <div className="error">{error}</div> }
     </form>
   
 )}
