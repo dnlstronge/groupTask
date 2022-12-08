2b14b08cce516ee6295986e37287d0c951b4c0b3
Dan Stronge <dnlstronge@gmail.com>
2022-12-08 14:16:44 +0000

    component outputs username & img

--- src/components/OnlineUsers.js
+++ src/components/OnlineUsers.js
@@ -1,11 +1,27 @@
+import { useCollection } from '../hooks/useCollection'
+
+//comps
+
+import Avatar from './Avatar'
+
 //styles
 import './OnlineUsers.css'
 
 
+
 export default function OnlineUsers() {
+  const { error, documents } = useCollection('users')
+
   return (
-    <div>
-        
+    <div className="user-list">
+        <h2>All Users</h2>
+        {error && <div className="error">{error}</div>}
+        {documents && documents.map( user => (
+          <div key={user.id}>
+            <span>{user.displayName}</span>
+            <Avatar src={user.photoURL}/>
+          </div>
+        ))}
     </div>
   )
 }
