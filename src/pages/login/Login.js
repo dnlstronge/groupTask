8ff4d47bd55cb6dfa0bb158a7af09df3b1f698de
Dan Stronge <dnlstronge@gmail.com>
2022-12-08 08:49:38 +0000

    create: start template for login - reuse some of signup

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,12 +1,39 @@
+import { useState } from 'react'
+import { useSignup } from '../../hooks/useSignup'
+
 // styles
 
 import styles from './Login.css'
 
 
 export default function Login() {
+  
   return (
-    <div>
-    Login
-    </div>
+    <form className="auth-form" onSubmit={handleSubmit}>
+      <h2>Sign up</h2>
+      <label>
+        <span>email:</span>
+        <input
+          required 
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}>
+        </input>
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          required 
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      
+      {!isPending && <button className="btn">Login</button>}
+      {isPending && <button className="btn" disabled>Loading...</button>}
+      {error && <div className="error">{error}</div>}
+    </form>
   )
 }
+
