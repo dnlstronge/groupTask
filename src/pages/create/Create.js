1639f4ea30b9bd81da704577b5a56cf0cee3e3ed
Dan Stronge <dnlstronge@gmail.com>
2022-12-08 15:26:27 +0000

    create: state for form

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,14 +1,25 @@
+import { useState } from 'react'
+
+
 // styles
 
 import styles from './Create.css'
 
 
 export default function Create() {
+  const [name, setName ] = useState('')
+  const [details, setDetails] = useState('')
+  const [dueDate, setDueDate] = useState('')
+  const [category, setCategory ] = useState('')
+  const [assignedUsers, setAssignedUsers] = useState([])
+
+
+
   return (
     <div className='create-form'>
     <h2 className="page-title">Create a new project</h2>
     <form>
-      
+
     </form>
     </div>
   )
