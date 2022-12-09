6876ac3678b78d8a9a9bb3fdeb01eb9962a99a48
Dan Stronge <dnlstronge@gmail.com>
2022-12-09 11:29:32 +0000

    create: add template to dashboard check: dashboard should output project name (clear)

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,12 +1,21 @@
+import { useCollection } from '../../hooks/useCollection'
+
+
 // styles
 
 import styles from './Dashboard.css'
 
 
 export default function Dashboard() {
+  const { documents, error } = useCollection('projects')
+
   return (
     <div>
-    Dashboard
+    <h2 className="page-title">Dashboard</h2>
+    {error && <p className="error">{error}</p>}
+    {documents && documents.map(doc => (
+      <p key={doc.id}>{doc.name}</p>
+    ))}
     </div>
   )
 }
