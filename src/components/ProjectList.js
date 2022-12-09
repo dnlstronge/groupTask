ec4d9f01d435ccd302ff52fc4f8a2c889ca0875d
Dan Stronge <dnlstronge@gmail.com>
2022-12-09 12:44:42 +0000

    create: project lists should now display name, dueDate and avatar(s) of assigned users (clear)

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,4 +1,5 @@
-
+import { Link } from 'react-router-dom'
+import Avatar from './Avatar'
 
 
 // styles
@@ -11,7 +12,21 @@ export default function ProjectList({ projects }) {
     <div>
         {projects.length === 0 && <p>There are no current projects</p>}
         {projects.map(project => (
-            <div key={project.id}>{project.name}</div>
+            <Link to={`/projects/${project.id}`} key={project.id}>
+                <h4>{project.name}</h4>
+                <p>Due by {project.dueDate.toDate().toDateString()}</p>
+                <div className="assigned-to">
+                <ul>
+                {project.assignedUsersList.map(user => (
+                    <li key={user.photoURL}>
+                      <Avatar src={user.photoURL}/>
+                    </li>
+                    
+                ))}
+                </ul>
+                </div>
+                
+            </Link>
         ))}
     </div>
   )
