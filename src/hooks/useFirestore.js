91524be39a944874abfc9f1e41b45ff05a663e3c
Dan Stronge <dnlstronge@gmail.com>
2022-12-10 13:47:37 +0000

    add new case to reducer

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -16,6 +16,8 @@ const firestoreReducer = (state, action) => {
       return { isPending: false, document: action.payload, success: true, error: null }
     case 'DELETED_DOCUMENT':
       return { isPending: false, document: null, success: true, error: null }
+    case 'UPDATED_DOCUMENT':
+      return { isPending: false, document: action.payload, success: true, error: null}
     case 'ERROR':
       return { isPending: false, document: null, success: false, error: action.payload }
     default:
@@ -85,6 +87,6 @@ export const useFirestore = (collection) => {
     return () => setIsCancelled(true)
   }, [])
 
-  return { addDocument, deleteDocument, response }
+  return { addDocument, deleteDocument, updateDocuments, response }
 
 }
