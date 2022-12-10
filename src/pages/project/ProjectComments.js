c2804c2b056434727317052e90b66c9a6cab5429
Dan Stronge <dnlstronge@gmail.com>
2022-12-10 12:23:34 +0000

    create: new comp - comments

---
+++ src/pages/project/ProjectComments.js
@@ -0,0 +1,11 @@
+import { useState } from "react"
+import { timestamp } from "../../firebase/config"
+import { useAuthContext } from '../../hooks/useAuthContext'
+
+
+
+export default function ProjectComments() {
+  return (
+    <div></div>
+  )
+}
