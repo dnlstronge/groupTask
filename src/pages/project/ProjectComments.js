9ad3553c250370f218cfcfe8acc28c1231573d43
Dan Stronge <dnlstronge@gmail.com>
2022-12-10 12:28:44 +0000

    create: add state for new comment

--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -5,7 +5,21 @@ import { useAuthContext } from '../../hooks/useAuthContext'
 
 
 export default function ProjectComments() {
+    const [newcomment, setNewComment] = useState('')
   return (
-    <div></div>
+    <div className="project-comments">
+    <h4>Project Comments</h4>
+    <form className="add-comments">
+        <label>
+            <span>Add new comment:</span>
+            <textarea
+              required
+              onChange{}>
+
+            </textarea>
+        </label>
+    </form> 
+
+    </div>
   )
 }
