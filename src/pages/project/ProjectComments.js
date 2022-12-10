3c5b587de9f5a5e922042a41cda0179e1720e133
Dan Stronge <dnlstronge@gmail.com>
2022-12-10 14:39:39 +0000

    create: comments now display in RT

--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import Avatar from "../../components/Avatar"
 import { timestamp } from "../../firebase/config"
 import { useAuthContext } from '../../hooks/useAuthContext'
 import { useFirestore } from "../../hooks/useFirestore"
@@ -31,6 +32,22 @@ export default function ProjectComments({ project }) {
   return (
     <div className="project-comments">
     <h4>Project Comments</h4>
+
+    <ul>
+      {project.comments.length > 0 && project.comments.map(comment => (
+        <li key={comment.id}>
+          <div className="comment-author">
+            <Avatar src={comment.photoURL}/>
+            <p>{comment.displayName}</p>
+          </div>
+          <div className="comment-date">Date Here</div>
+          <div className="comment-content">
+            <p>{comment.content}</p>
+          </div>
+        </li>
+      ))}
+    </ul>
+
     <form className="add-comment" onSubmit={handleSubmit}>
         <label>
             <span>Add new comment:</span>
