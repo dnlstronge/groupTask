02bbd07e6a145fcf29fbceb93775afaea50f413a
Dan Stronge <dnlstronge@gmail.com>
2022-12-11 10:53:04 +0000

    create: creator of project can now mark complete(delete)

--- src/pages/project/ProjectSummary.js
+++ src/pages/project/ProjectSummary.js
@@ -1,41 +1,40 @@
-import Avatar from '../../components/Avatar'
-import { useFirestore } from '../../hooks/useFirestore'
-import { useAuthContext } from '../../hooks/useAuthContext'
-import { useHistory } from 'react'
-
+import Avatar from "../../components/Avatar"
+import { useFirestore } from "../../hooks/useFirestore"
+import { useHistory } from 'react-router-dom'
+import { useAuthContext } from "../../hooks/useAuthContext"
 
 export default function ProjectSummary({ project }) {
   const { deleteDocument } = useFirestore('projects')
   const { user } = useAuthContext()
   const history = useHistory()
 
-  const handleClick = (e) => {
+  const handleClick = () => {
     deleteDocument(project.id)
-    
-
+    history.push('/')
   }
 
   return (
     <div>
-        <div className="project-summary">
-            <h2 className="page-title">{project.name}</h2>
-            <p>Created by: {project.createdBy.displayName}</p>
-            <p className="due-date">Project due by: {project.dueDate.toDate().toDateString()}</p>
-            <p className="details">
-            {project.details}
-            </p>
-            <h4>Project is assigned to:</h4>
-            <div className="assigned-users">
-              {project.assignedUsersList.map(user => (
-                <div key={user.id}>
-                <Avatar src={user.photoURL}/>
-                </div>
-            ))}
+      <div className="project-summary">
+        <h2 className="page-title">{project.name}</h2>
+        <p className="due-date">
+          Project due by {project.dueDate.toDate().toDateString()}
+        </p>
+        <p className="details">
+          {project.details}
+        </p>
+        <h4>Project assigned to:</h4>
+        <div className="assigned-users">
+          {project.assignedUsersList.map(user => (
+            <div key={user.id}>
+              <Avatar src={user.photoURL} />
             </div>
+          ))}
         </div>
-        {user.uid === project.createdBy.id && (
-            <button className="btn" onClick={handleClick}>Mark as complete</button>
-            )}
+      </div>
+      {user.uid === project.createdBy.id && (
+        <button className="btn" onClick={handleClick}>Mark as Complete</button>
+      )}
     </div>
   )
 }
