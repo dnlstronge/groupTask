d1e1eed78f586aa289ae546afe80799a04200462
Dan Stronge <dnlstronge@gmail.com>
2022-12-11 12:24:20 +0000

    create: projects now filter based on button select

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,35 +1,31 @@
-import ProjectList from '../../components/ProjectList'
 import { useCollection } from '../../hooks/useCollection'
 import { useState } from 'react'
 import { useAuthContext } from '../../hooks/useAuthContext'
 
-
-// styles
-
-import styles from './Dashboard.css'
+// components
+import ProjectList from '../../components/ProjectList'
 import ProjectFilter from './ProjectFilter'
 
+// styles
+import './Dashboard.css'
 
 export default function Dashboard() {
   const { user } = useAuthContext()
   const { documents, error } = useCollection('projects')
-  const [currentFilter, setCurrentFilter] = useState('all')
+  const [filter, setFilter] = useState('all')
 
   const changeFilter = (newFilter) => {
-    setCurrentFilter(newFilter)
+    setFilter(newFilter)
   }
-
-
-  // filtered 
-
-  const projects = documents.filter((document) => {
-    switch (currentFilter) {
+  
+  const projects = documents ? documents.filter(document => {
+    switch(filter) {
       case 'all':
         return true
-      case 'mine': 
+      case 'mine':
         let assignedToMe = false
-        document.assignedUserList.forEach((u) => {
-          if(user.uid === u.id ) {
+        document.assignedUsersList.forEach(u => {
+          if(u.id === user.uid) {
             assignedToMe = true
           }
         })
@@ -38,23 +34,20 @@ export default function Dashboard() {
       case 'design':
       case 'sales':
       case 'marketing':
-        console.log(document.category, currentFilter)
-        return document.category === currentFilter 
-
+        console.log(document.category, filter)
+        return document.category === filter
+      default:
+        return true
     }
-      
-
-  })
-
+  }) : null
 
   return (
     <div>
-    <h2 className="page-title">Dashboard</h2>
-    {error && <p className="error">{error}</p>}
-    {documents && ( <ProjectFilter 
-      currentFilter={currentFilter} changeFilter={changeFilter}/>
-      )}
-    {documents && <ProjectList projects={projects}/>}
+      <h2 className="page-title">Dashboard</h2>
+      {error && <p className="error">{error}</p>}
+      {documents && <ProjectFilter changeFilter={changeFilter} />}
+      {projects && <ProjectList projects={projects} />}
     </div>
   )
 }
+
