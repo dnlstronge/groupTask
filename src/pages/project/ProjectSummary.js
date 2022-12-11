9b9fffe3e9ecd7fa8ae7c8200bd7df4164fbb1d5
Dan Stronge <dnlstronge@gmail.com>
2022-12-11 14:08:45 +0000

    restored delete button functionality

--- src/pages/project/ProjectSummary.js
+++ src/pages/project/ProjectSummary.js
@@ -33,9 +33,9 @@ export default function ProjectSummary({ project }) {
           ))}
         </div>
       </div>
-       
+       {user.uid === project.createdBy.id && (
         <button className="btn" onClick={handleClick}>Mark as Complete</button>
-     
+      )}
     </div>
   )
 }
